83534d87a0665f5994d4a95a23914ad60c540580
AyeOne11 <thesilentsector@proton.me>
2026-01-28 22:49:41 -0500

    Update server.js
    Signed-off-by: AyeOne11 <thesilentsector@proton.me>

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.post('/api/entries', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.PORT || 3000;
 app.listen(PORT, () => console.log(`The Gateway is active on port ${PORT}`));
+
 
